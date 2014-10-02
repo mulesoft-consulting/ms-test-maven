c2ce11c80a53dd71601603c03146e962a26b8ce2
adamdavis <adamdavis@172.16.30.76>
2014-10-02 15:43:58 +0100

    IT test now pulls port from property file but still expects the app and test to be run on localhost

--- src/test/java/tests/integration/ITSimpleHttp.java
+++ src/test/java/tests/integration/ITSimpleHttp.java
@@ -1,9 +1,11 @@
 package tests.integration;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
-
+import java.util.Properties;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.ClientProtocolException;
@@ -14,20 +16,33 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class ITSimpleHttp  {
-	 
+public class ITSimpleHttp {
+
+	private String getPort() throws IOException {
+		Properties prop = new Properties();
+		InputStream is = getClass().getClassLoader().getResourceAsStream(
+				"mule-app.properties");
+		prop.load(is);
+		if (is==null){
+			throw new FileNotFoundException("mule-app.properties not found on classpath");
+		}
+		return prop.getProperty("http.port");
+	}
+
 	@Test
-    public void test() throws ClientProtocolException, IOException {   
+	public void test() throws ClientProtocolException, IOException {
+		String port = getPort();
 		HttpClient client = new DefaultHttpClient();
-		 HttpGet request = new HttpGet("http://localhost:8085/test");
-	        HttpResponse response = client.execute(request);
-	        BufferedReader rd = new BufferedReader (new InputStreamReader(response.getEntity().getContent()));
-	        String line = "";
-	        StringBuffer output = new StringBuffer();
-	        while ((line = rd.readLine()) != null) {
-	        	output.append(line);
-	        }
-	        assertEquals(output.toString(), "Hello World");
-    }
-	
+		HttpGet request = new HttpGet("http://localhost:"+port+"/test");
+		HttpResponse response = client.execute(request);
+		BufferedReader rd = new BufferedReader(new InputStreamReader(response
+				.getEntity().getContent()));
+		String line = "";
+		StringBuffer output = new StringBuffer();
+		while ((line = rd.readLine()) != null) {
+			output.append(line);
+		}
+		assertEquals(output.toString(), "Hello World");
+	}
+
 }
