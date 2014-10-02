b0eaa4c6d86435fe96d4a02900c4567c05309c8f
adamdavis <adamdavis@172.16.30.76>
2014-10-02 15:10:31 +0100

    Added a Unit test that does not test anything

---
+++ src/test/java/tests/unit/SimpleUnitTest.java
@@ -0,0 +1,14 @@
+package tests.unit;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SimpleUnitTest {
+
+	@Test
+    public void test() {
+		assertEquals(true, true);
+	}
+	
+}
